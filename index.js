7c0a4a8fa499efed75781e13bfc3d5133d7e9c3d
Colin Hoernig <Hoernig.Colin@gmail.com>
2015-10-17 17:53:14 -0500

    Create list of riders (entries).  Create store subscription and emit on state change.

--- index.js
+++ index.js
@@ -2,4 +2,10 @@ import makeStore from './src/store';
 import startServer from './src/server';
 
 export const store = makeStore();
-startServer();
+startServer(store);
+
+store.dispatch({
+  type: 'SET_ENTRIES',
+  entries: require('./entries.json')
+});
+store.dispatch({ type: 'NEXT' });
--- src/server.js
+++ src/server.js
@@ -1,5 +1,29 @@
 import Server from 'socket.io';
 
-export default function startServer() {
+
+/**
+ * 1. A client sends an action to the server
+ * 2. The server hands the action to the Redux Store
+ * 3. The Store calls the reducer and the reducer
+ *    executes the logic related to the action
+ * 4. The Store updates its state based on the return value of the reducer
+ * 5. The Store executes the listener function subscribed by the server
+ * 6. The server emits a 'state' event
+ * 7. All connected clients - including the one that initiated the
+ *    original action - receive the new state.
+ */
+export default function startServer(store) {
   const io = new Server().attach(8001);
+
+  // This should be optimized, as transferring the whole state with every
+  // change could end up causing a lot of data transfer.  Maybe send the relevant
+  // subset, or diff of the new/old state, etc.
+  store.subscribe(
+    () => io.emit('state', store.getState().toJS())
+  );
+
+  io.on('connection', (socket) => {
+    socket.emit('state', store.getState().toJS());
+    socket.on('action', store.dispatch.bind(store));
+  });
 }
