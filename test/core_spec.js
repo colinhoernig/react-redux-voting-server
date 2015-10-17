12f5bf35512a88959da69e12edd70e120100ba20
Colin Hoernig <Hoernig.Colin@gmail.com>
2015-10-17 00:35:48 -0500

    Implement next method test spec and next src/core method.

--- test/core_spec.js
+++ test/core_spec.js
@@ -1,5 +1,6 @@
 import { expect } from 'chai';
 import { List, Map } from 'immutable';
+import { setEntries, next } from '../src/core';
 
 import { setEntries } from '../src/core';
 
@@ -9,19 +10,43 @@ describe('application logic', () => {
 
     it('adds the entries to the state', () => {
       const state = Map();
-      const entries = List.of(
-        'Travis Rice',
-        'Joe Sexton',
-        'Jeremy Jones',
-        'Alex Andrews'
-      );
+      const entries = List.of('Travis Rice', 'Joe Sexton');
+      const nextState = setEntries(state, entries);
+
+      expect(nextState).to.equal(Map({ 'Travis Rice', 'Joe Sexton' }));
+    });
+
+    it('converts to immutable', () => {
+      const state = Map();
+      const entries = List.of('Travis Rice', 'Joe Sexton');
       const nextState = setEntries(state, entries);
 
       expect(nextState).to.equal(Map({
-        'Travis Rice',
-        'Joe Sexton',
-        'Jeremy Jones',
-        'Alex Andrews'
+        entries: List.of(
+          'Travis Rice',
+          'Joe Sexton'
+        );
+      }));
+    });
+
+  });
+
+  describe('next', () => {
+
+    it('takes the next two entries under vote', () => {
+      const state = Map({
+        entries: List.of('Jeremy Jones', 'Alex Andrews')
+      });
+      const nextState = next(state);
+
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of(
+            'Jeremy Jones',
+            'Alex Andrews'
+          ),
+          entries: List.of('Nicolas Müller')
+        })
       }));
     });
 
